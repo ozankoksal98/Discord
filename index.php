8888580e4f1f9050aee83838fe960592602a2e88
ozankoksal98 <ozankoksal@hotmail,com>
2020-09-16 17:34:20 +0300

    0

--- index.php
+++ index.php
@@ -1,114 +1,145 @@
 <?php
      header("Access-Control-Allow-Origin: *");
-    function getReturnAddress()
-    {
-        $returnURL = "http://localhost/curl/Discord/index.php?";
-        $value= array();
-        foreach ($_GET as $k=>$v) {
-            $value[] = $k."=".$v;
-        }
-        return $returnURL .= implode("&", $value);
-    }
-   
     
+    require_once "src/AuthorizeBot.php";
+    require_once "src/DiscordIntegration.php";
 
-    include_once "AuthorizeBot.php";
-    include_once "DiscordIntegration.php";
-    $token = "";
-    $clientID = "";
-    /*
-    if (!isset($_GET["type"])) {
-        $bot = new AuthorizeBot($clientID, "http://localhost/curl/Discord/index.php", $token);
-    }
-    */
-    $integration = new DiscordIntegration($token, 744853637385420921);
+    $token = "NzQ1NjcwOTUzMTc4MjM0OTQw.Xz1KMQ.GFrdHi6lengxhqp4sAYvC-WzFz0";
+    $clientID = "745670953178234940";
+    $integration = new DiscordIntegration($token, 744853637385420921, "fb164eb729c73fd5456f005f93a2715c");
 
 
-    if (isset($_GET["type"])) {
-        if ($_GET["type"]=="forms") {
-            $filteredForms= [];
-            foreach ($integration->getForms() as $k) {
-                $filteredForms[] = ["id"=>$k["id"],"title"=>$k["title"],"url" => $k["url"]];
-            }
-            echo json_encode($filteredForms);
-        } elseif ($_GET["type"]=="channels") {
-            $ch = json_decode($integration->getApi()->getGuildChannels(), true);
-            $filteredChannels = array();
-            foreach ($ch as $k) {
-                if ($k["type"]==0 || $k["type"]==5) {
-                    $filteredChannels[] =["id"=>$k["id"],"type"=>$k["type"],"title"=>$k["name"],"parent_id"=>$k["parent_id"]];
-                }
-            }
-            echo json_encode($filteredChannels);
-        } elseif ($_GET["type"]=="fields") {
-            $filteredQuestions = [];
-            $skippedFields = ["control_button","control_head","control_captcha","control_divider","control_text","control_image","control_inline","control_widget"];
-            foreach ($integration->getFormNew($_GET["formID"])->getQuestions() as $k) {
-                if (isset($k["text"])) {
-                    if (!in_array($k["type"], $skippedFields)) {
-                        $filteredQuestions [] = ["id" => $k["order"],"title" => $k["text"], "type"=> $k["type"]];
-                    }
-                } else {
-                    $filteredQuestions [] = ["id" => $k["order"],"title" => "name", "type"=> $k["type"]];
-                }
-            }
-            
-            echo json_encode($filteredQuestions);
-        } elseif ($_GET["type"]=="submissions") {
-            $filteredSubmissions = [];
-            foreach ($integration->getFormNew($_GET["formID"])->getSubmissions() as $k) {
-                $submName;
-                foreach ($k["answers"] as $answer) {
-                    if ($answer["type"] == "control_fullname") {
-                        $submName = "Submission by : ".$answer["prettyFormat"];
-                        break;
-                    }
-                }
-                $filteredSubmissions [] = ["id" => $k["id"],"title" => $submName];
-            }
-            
-            echo json_encode($filteredSubmissions);
+if (isset($_GET["type"])) {
+    if ($_GET["type"]=="forms") {
+        $filteredForms= [];
+        foreach ($integration->getForms() as $k) {
+            $filteredForms[] = ["id"=>$k["id"],"title"=>$k["title"],"url" => $k["url"]];
         }
-    } else {
-        $post = json_decode(file_get_contents("php://input"), true);
-        foreach ($post["channels"] as $channel) {
-            foreach ($post["submissions"] as $subm) {
-                $integration->getApi()->sendMessage($channel, "", $integration->buildMessage($post["fields"], $post["form"], $subm));
+        echo json_encode($filteredForms);
+    } elseif ($_GET["type"]=="channels") {
+        $ch = json_decode($integration->getApi()->getGuildChannels(), true);
+        $filteredChannels = array();
+        foreach ($ch as $k) {
+            if ($k["type"]==0 || $k["type"]==5) {
+                $filteredChannels[] =["id"=>$k["id"],"type"=>$k["type"],"title"=>$k["name"],"parent_id"=>$k["parent_id"]];
             }
         }
-        //print return value
-        echo(json_encode($post));
-        //connect to database
+        $guild_members = json_decode($integration->getApi()->getGuildMembers(), true);
+        foreach ($guild_members as $k) {
+            if (!isset($k["user"]["bot"])) {
+                $filteredChannels[] =["id"=>$k["user"]["id"],"type"=>"1","title"=>$k["user"]["username"],"parent_id"=>"0"];
+            }
+        }
+        echo json_encode($filteredChannels);
+    } elseif ($_GET["type"]=="fields") {
+        $filteredQuestions = [];
+        $skippedFields = ["control_button","control_head","control_captcha","control_divider","control_text","control_image","control_inline","control_widget"];
+        foreach ($integration->getFormNew($_GET["formID"])->getQuestions() as $k) {
+            if (isset($k["text"])) {
+                if (!in_array($k["type"], $skippedFields)) {
+                    $filteredQuestions [] = ["id" => $k["order"],"title" => $k["text"], "type"=> $k["type"]];
+                }
+            } else {
+                $filteredQuestions [] = ["id" => $k["order"],"title" => "name", "type"=> $k["type"]];
+            }
+        }
+            
+        echo json_encode($filteredQuestions);
+    } elseif ($_GET["type"]=="submissions") {
+        $filteredSubmissions = [];
+        foreach ($integration->getFormNew($_GET["formID"])->getSubmissions() as $k) {
+            $submName;
+            foreach ($k["answers"] as $answer) {
+                if ($answer["type"] == "control_fullname" && isset($answer["prettyFormat"])) {
+                    $submName = "Submission by : ".$answer["prettyFormat"];
+                    break;
+                }
+            }
+            $filteredSubmissions [] = ["id" => $k["id"],"title" => $submName];
+        }
+            
+        echo json_encode($filteredSubmissions);
+    } elseif ($_GET["type"]=="history") {
         $conn = mysqli_connect("localhost", "ozan", "password", "discord_integration");
-        //check connetion
+        $result = mysqli_query($conn, "SELECT id,user_id,guild_id,form_id,channels,users,fields,notes,submissions,created_at FROM queries WHERE user_id = ".$_GET['user_id']);
+        $val = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach ($val as $k=>$v) {
+            $ch = json_decode($v["channels"], true);
+            $form = json_decode($v["form_id"], true);
+            $field =json_decode($v["fields"], true);
+            $sub = json_decode($v["submissions"], true);
+            $users =  json_decode($v["users"], true);
+            $notes = json_decode($v["notes"], true);
+            $val[$k]["channels"] = $ch;
+            $val[$k]["form_id"] = $form;
+            $val[$k]["fields"] = $field;
+            $val[$k]["submissions"] = $sub;
+            $val[$k]["users"] = $users;
+            $val[$k]["notes"] = $notes;
+            //$k["channels"] = "asdfasd";
+        }
+        echo json_encode($val);
+    }
+} else {
+    $post = json_decode(file_get_contents("php://input"), true);
+    $fields = [];
+    foreach (($post["fields"]) as $field) {
+        $fields[] =  $field["id"];
+    }
+    if ($post["type"] == "preview") {
+        echo  $integration->buildMessage($fields, $post["form"][0]["id"], $post["submissions"][0]["id"], $post["notes"]);
+    } elseif ($post["type"] == "submit") {
+        print_r($post);
+        foreach (($post["channels"]) as $channel) {
+            foreach (($post["submissions"]) as $subm) {
+                $integration->getApi()->sendMessage($channel["id"], "", $integration->buildMessage($fields, $post["form"][0]["id"], $subm["id"], $post["notes"]));
+            }
+        }
+        
+        foreach (($post["users"]) as $user) {
+            $dm_channel = json_decode($integration->getApi()->createDmChannel($user["id"]), true)["id"];
+            foreach (($post["submissions"]) as $subm) {
+                $integration->getApi()->sendMessage($dm_channel, "", $integration->buildMessage($fields, $post["form"][0]["id"], $subm["id"], $post["notes"]));
+            }
+        }
+        
+
+        $conn = mysqli_connect("localhost", "ozan", "password", "discord_integration");
         if (!$conn) {
             echo "connection failed: ". mysqli_connect_error();
         } else {
             //post query
-            $username = mysqli_real_escape_string($conn, $post["username"]);
-            $id = mysqli_real_escape_string($conn, $post["id"]);
-            $guild_id = mysqli_real_escape_string($conn, $post["guild_id"]);
-            $form = mysqli_real_escape_string($conn, $post["form"]);
-            $channels = mysqli_real_escape_string($conn, implode(",",$post["channels"]));
-            $fields = mysqli_real_escape_string($conn,implode(",",$post["fields"]));
-            $submissions = mysqli_real_escape_string($conn,implode(",",$post["submissions"]));
-            $sql = "INSERT INTO queries(user_id,guild_id,form_id,channels,fields,submissions)
-            VALUES ('$id','$guild_id','$form','$channels','$fields','$submissions')";
-            mysqli_query($conn,$sql);
+            $username =  $post["username"];
+            $id =  $post["id"];
+            $guild_id =  $post["guild_id"];
+            $form =  json_encode($post["form"]);
+            $channels =  json_encode($post["channels"]);
+            $fields = json_encode($post["fields"]);
+            $submissions = json_encode($post["submissions"]);
+            $users = json_encode($post["users"]);
+            $notes = json_encode($post["notes"]);
+            $sql = "INSERT INTO queries(user_id,guild_id,form_id,channels,users,fields,notes,submissions)
+            VALUES ('$id','$guild_id','$form','$channels','$users','$fields','$notes','$submissions')";
+            mysqli_query($conn, $sql);
             echo "query error". mysqli_error($conn);
             //check if user already exists
             //post user to users table
-            $user_ids = mysqli_fetch_all(mysqli_query($conn, "SELECT user_id FROM users")) ;
+            $user_ids = mysqli_fetch_all(mysqli_query($conn, "SELECT user_id FROM users"));
             if (!in_array($id, $user_ids[0])) {
-                if (mysqli_query($conn,"INSERT INTO users(user_id,user_name,guild_id)
-                VALUES ('$id','$username','$guild_id')")) {
+                if (mysqli_query(
+                    $conn,
+                    "INSERT INTO users(user_id,user_name,guild_id)
+                VALUES ('$id','$username','$guild_id')"
+                )
+                ) {
                     echo "query successful";
                 } else {
-                    echo "query error". mysqli_error($conn);
+                    echo "query error ,cant add user". mysqli_error($conn);
                 }
             }
         }
     }
+}
    
    
     /*
--- src/OldScript.php
+++ src/OldScript.php
@@ -2,19 +2,19 @@
 
 <?php
  header("Access-Control-Allow-Origin: *");
-    function getReturnAddress()
-    {
-        $returnURL = "http://localhost/curl/Discord/index.php?";
-        $value= array();
-        foreach ($_GET as $k=>$v) {
-            $value[] = $k."=".$v;
-        }
-        return $returnURL .= implode("&", $value);
+function getReturnAddress()
+{
+    $returnURL = "http://localhost/curl/Discord/index.php?";
+    $value= array();
+    foreach ($_GET as $k=>$v) {
+        $value[] = $k."=".$v;
     }
+    return $returnURL .= implode("&", $value);
+}
    
 
-    include_once "AuthorizeBot.php";
-    include_once "DiscordIntegration.php";
+    require_once "AuthorizeBot.php";
+    require_once "DiscordIntegration.php";
     $token = "";
     $formID = "202251323327039";
     $clientID = "";
@@ -32,17 +32,17 @@
     //choose channel
     echo "Choose channel(s) to send submissions: <br>";
     echo '<form action="'.getReturnAddress().'" method="post">';
-    foreach ($integration->getApi()->getTextChannels() as $channel => $v) {
-        echo  '<div class="checkbox">
+foreach ($integration->getApi()->getTextChannels() as $channel => $v) {
+    echo  '<div class="checkbox">
         <label><input type="checkbox" name="'.$v["id"].'" value = "channel" ">  '.$v["name"].'</label>
         </div>';
-    };
+};
     $chosenChannels = [];
-    foreach ($_POST as $channel =>$v) {
-        if ($v=="channel") {
-            $chosenChannels[] = $channel;
-        }
+foreach ($_POST as $channel =>$v) {
+    if ($v=="channel") {
+        $chosenChannels[] = $channel;
     }
+}
 
     echo implode(",", $chosenChannels);
     echo '</div><div class="col bg-danger text-white">';
@@ -52,49 +52,49 @@
     $skippedFields = ["control_button","control_head","control_captcha","control_divider","control_text","control_image","control_inline"];
     $type;
     $usedFields = array();
-    foreach ($integration->getForm()->getQuestions() as $question) {
-        if (!in_array($question["type"], $skippedFields)) {
-            echo  '<div class="checkbox">
+foreach ($integration->getForm()->getQuestions() as $question) {
+    if (!in_array($question["type"], $skippedFields)) {
+        echo  '<div class="checkbox">
             <label><input type="checkbox" name="'.$question["order"].'" value = "questions" >  '.$question["text"].'</label>
             </div>';
-        } elseif ($question["type"]=="control_inline") {
-            echo  '<div class="checkbox">
+    } elseif ($question["type"]=="control_inline") {
+        echo  '<div class="checkbox">
             <label><input type="checkbox" name="'.$question["order"].'" value = "questions" >  Fill the blanks </label>
             </div>';
-        }
     }
+}
     $chosenQuestions = [];
-    foreach ($_POST as $question =>$v) {
-        if ($v=="questions") {
-            $chosenQuestions[] = $question;
-        }
+foreach ($_POST as $question =>$v) {
+    if ($v=="questions") {
+        $chosenQuestions[] = $question;
     }
+}
     echo implode(",", $chosenQuestions);
 
     echo '</div>
     <div class="col bg-warning ">';
     //choose submissions
      echo "Choose submissions :<br>";
-    foreach ($integration->getForm()->getSubmissions() as $subm) {
-        $submID = $subm["id"];
-        $submName;
-        foreach ($subm["answers"] as $answer) {
-            if ($answer["type"] == "control_fullname") {
-                $submName = "Submission by : ".$answer["prettyFormat"];
-                break;
-            }
+foreach ($integration->getForm()->getSubmissions() as $subm) {
+    $submID = $subm["id"];
+    $submName;
+    foreach ($subm["answers"] as $answer) {
+        if ($answer["type"] == "control_fullname") {
+            $submName = "Submission by : ".$answer["prettyFormat"];
+            break;
         }
-        echo  '<div class="checkbox">
+    }
+    echo  '<div class="checkbox">
         <label><input type="checkbox" name="'.$submID.'" value = "submissions" >  '.$submName.'</label>
         </div>';
-    }
+}
 
     $chosenSubmissions = [];
-    foreach ($_POST as $submission =>$v) {
-        if ($v=="submissions") {
-            $chosenSubmissions[] = $submission;
-        }
+foreach ($_POST as $submission =>$v) {
+    if ($v=="submissions") {
+        $chosenSubmissions[] = $submission;
     }
+}
     echo implode(",", $chosenSubmissions);
 
     echo ' </div> </div> </div>
@@ -103,8 +103,8 @@
     </div></form></div>
     ';
 
-    foreach ($chosenChannels as $channel) {
-        foreach ($chosenSubmissions as $subm) {
-            $integration->getApi()->sendMessage($channel, "", $integration->buildMessage($chosenQuestions, $formID, $subm));
-        }
+foreach ($chosenChannels as $channel) {
+    foreach ($chosenSubmissions as $subm) {
+        $integration->getApi()->sendMessage($channel, "", $integration->buildMessage($chosenQuestions, $formID, $subm));
     }
+}
